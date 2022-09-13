16b896bdce56eb6a14f59b83e2c0bcc06600bdf9
shekhar1710 <sawantshekhar1717@gmail.com>
2022-09-13 15:11:04 +0530

    Code for students in a department.

--- Shekhar/src/com/first/projet/ArrayList.java
+++ Shekhar/src/com/first/projet/ArrayList.java
@@ -1,18 +1,35 @@
 package com.first.projet;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class ArrayList {
 
 	public static void main(String[] args) {
 		
-		List<String> list = Arrays.asList("Shekhar","Rushikesh","Gayatri","Krushnat","Rohit");
+		List<String> mechanical = Arrays.asList("Shekhar","Krushnat");
+		List<String> civil = Arrays.asList("Rushikesh","Shrikant");
+		List<String> electronics = Arrays.asList("Gayatri","Arav");
+		List<String> computer = Arrays.asList("Rohit","Mohit");
 
-		Iterator<String> itr = list.iterator();
-		while(itr.hasNext()) {
-			System.out.println(itr.next());
+		
+		Map<String, List<String>> map = new HashMap<>();
+		
+		map.put("Mechanical :", mechanical);
+		map.put("Civil :", civil);
+		map.put("Electronics :", electronics);
+		map.put("Computer :", computer);
+		
+		
+		Set<String> set = map.keySet();
+		for(Object o : set) {
+			
+			System.out.println(o);
+			System.out.println(map.get(o));
 		}
 	}
 
